998f9b8b0db6fae47e4a27144fc8da112cb4c7b9
Angel Salas Calvo <angelsalascalvo@gmail.com>
2019-06-24 17:57:47 +0200

    Lunes 24/06/2019

--- P07WebViewAngelSalas/app/src/main/java/com/example/angel/p07webviewangelsalas/ActivityWeb.java
+++ P07WebViewAngelSalas/app/src/main/java/com/example/angel/p07webviewangelsalas/ActivityWeb.java
@@ -1,15 +1,33 @@
 package com.example.angel.p07webviewangelsalas;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 public class ActivityWeb extends AppCompatActivity {
     private WebView wvNavega;
+    private Button pixel;
+    private ImageView foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +35,23 @@ public class ActivityWeb extends AppCompatActivity {
         setContentView(R.layout.activity_web);
         //Referencias de los objetos de la activity
         wvNavega = (WebView) findViewById(R.id.wvNavegador);
+        pixel = (Button) findViewById(R.id.pixel);
+        foto = (ImageView) findViewById(R.id.foto);
 
+        pixel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = takeScreenshot();
+                foto.setImageBitmap(bitmap);
+                int pixel = bitmap.getPixel(500,800);
+
+                int redValue = Color.red(pixel);
+                int blueValue = Color.blue(pixel);
+                int greenValue = Color.green(pixel);
+
+                mostrar(redValue+", "+blueValue+", "+greenValue);
+            }
+        });
         //Obtener url enviada desde la activity main
         String url = getIntent().getStringExtra("url");
 
@@ -25,8 +59,45 @@ public class ActivityWeb extends AppCompatActivity {
         wvNavega.setWebViewClient(new WebViewClient()); //Activar el navegador WebView
         wvNavega.loadUrl("http://"+url); //Asignar url
 
+        wvNavega.setFindListener(new WebView.FindListener() {
+
+            @Override
+            public void onFindResultReceived(int activeMatchOrdinal, int numberOfMatches, boolean isDoneCounting) {
+                Toast.makeText(getApplicationContext(), "Matches: " + numberOfMatches, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
         //NO OLVIDARSE DE SOLICITAR PERMISO DE ACCESO A INTERNET EN AndroidManifest.xml
         //  <uses-permission android:name="android.permission.INTERNET"/>
+    }
+
+    public void mostrar(String s){
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    public Bitmap takeScreenshot() {
+        View rootView = findViewById(android.R.id.content).getRootView();
+        rootView.setDrawingCacheEnabled(true);
+        Bitmap b = rootView.getDrawingCache();
+        return b;
+    }
+
+
+
+
+
+    public void buscar(View view){
+        wvNavega.findAllAsync("https://www.hola.com");
+
+        Toast.makeText(getApplicationContext(), "reee", Toast.LENGTH_LONG).show();
+    }
+
+    public void siguiente(View view){
+        wvNavega.findNext(true);
     }
 
     /**
--- P07WebViewAngelSalas/app/src/main/java/com/example/angel/p07webviewangelsalas/MainActivity.java
+++ P07WebViewAngelSalas/app/src/main/java/com/example/angel/p07webviewangelsalas/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.angel.p07webviewangelsalas;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etUrlM;
@@ -16,6 +23,58 @@ public class MainActivity extends AppCompatActivity {
 
         //Referencias
         etUrlM = (EditText) findViewById(R.id.etUrl);
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.INTERNET)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.INTERNET}, 1234);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+
+
+        }
+
+
+/*
+// Obtain MotionEvent object
+        long downTime = SystemClock.uptimeMillis();
+        long eventTime = SystemClock.uptimeMillis() + 100;
+        float x = 550.0f;
+        float y = 600.0f;
+// List of meta states found here: developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+        int metaState = 0;
+        MotionEvent motionEvent = MotionEvent.obtain(
+                downTime,
+                eventTime,
+                MotionEvent.ACTION_UP,
+                x,
+                y,
+                metaState
+        );
+
+        SystemClock.sleep(5000);
+        Toast.makeText(this, "sss",Toast.LENGTH_SHORT).show();
+// Dispatch touch event to view
+        this.dispatchTouchEvent(motionEvent);
+        */
     }
 
     //Metodo para el clic del boton "ir al sitio web"
